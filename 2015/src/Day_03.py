d7f1d3ed2bb998ec20eecb374f6c7f6b65468465
Harry Vallance <contact@harryvallance.com>
2025-10-15 22:39:23 +0100

    Day 03 Part 1

--- 2015/src/Day_03.py
+++ 2015/src/Day_03.py
@@ -1,4 +1,31 @@
 from utils import read_file
 
-values = read_file(3, str, True)
+values = read_file(3, str, False)
 
+instructions = list(values[0])
+
+visited = {}
+x, y = 0, 0
+
+visited[(x, y)] = 1
+
+for instruction in instructions:
+    if instruction == '>':
+        x += 1
+    if instruction == '<':
+        x -= 1
+    if instruction == 'v':
+        y -= 1
+    if instruction == '^':
+        y += 1
+    if (x,y) in visited:
+        visited[(x, y)] = visited[(x, y)] + 1
+    else:
+        visited[(x, y)] = 1
+
+received_present = 0
+for house in visited:
+    if visited[house] >= 1:
+        received_present += 1
+
+print(f'Part 1: {received_present}')
