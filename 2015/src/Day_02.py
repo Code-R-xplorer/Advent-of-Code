0784f2a1c289949c84f9e117aad14d515db462e6
Harry Vallance <contact@harryvallance.com>
2025-10-15 19:08:31 +0100

    Day 02 Part 1

--- 2015/src/Day_02.py
+++ 2015/src/Day_02.py
@@ -1,4 +1,17 @@
 from utils import read_file
 
-values = read_file(2, str, True)
+values = read_file(2, str, False)
 
+wrapping_paper_needed = 0
+
+for value in values:
+    ls, ws, hs = value.split('x')
+    l, w, h = int(ls), int(ws), int(hs)
+    side_a = 2*(l*w)
+    side_b = 2*(w*h)
+    side_c = 2*(h*l)
+    smallest_side = min(side_a/2, side_b/2, side_c/2)
+    square_feet_needed = (side_a+side_b+side_c) + int(smallest_side)
+    wrapping_paper_needed += square_feet_needed
+
+print(f'Part 1: {wrapping_paper_needed}')
