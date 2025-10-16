23265f9da1ee54b91b28155197c021f51dde5d2e
Harry Vallance <contact@harryvallance.com>
2025-10-16 21:32:11 +0100

    Day 04 Part 2

--- 2015/src/Day_04.py
+++ 2015/src/Day_04.py
@@ -12,3 +12,11 @@ for i in range(1000000000):
     if leading_zeros == 5:
         print(f'Part 1: {i}')
         break
+
+for i in range(1000000000):
+    new_secret = secret + str(i)
+    res = hashlib.md5(new_secret.encode()).hexdigest()
+    leading_zeros = len(res) - len(res.lstrip('0'))
+    if leading_zeros == 6:
+        print(f'Part 2: {i}')
+        break
