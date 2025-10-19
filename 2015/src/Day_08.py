e176f2e96e264463b229ba71bae04ad8502ffa6a
Harry Vallance <contact@harryvallance.com>
2025-10-19 11:13:17 +0100

    Day 08 Part 1

--- 2015/src/Day_08.py
+++ 2015/src/Day_08.py
@@ -1,4 +1,20 @@
 from utils import read_file
+import re
 
-values = read_file(8, str, True)
+values = read_file(8, str, False)
 
+total = 0
+
+for value in values:
+    line = value
+    line = line.strip('"')
+    memory_count = len(value)
+    hexes = re.findall(r'\\x[0-9A-Fa-f]{2}', value)
+    if len(hexes) > 0:
+        for hex in hexes:
+            line = line.replace(hex, bytes(hex, "utf-8").decode("unicode_escape"))
+    line = re.sub(r'\\(["\\])', r'\1', line)
+    string_count = len(line)
+    total = total + (memory_count - string_count)
+
+print(f'Part 1: {total}')
