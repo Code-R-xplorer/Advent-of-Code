64cc8aac478c7fce384f6da08c084f925320a5d8
Harry Vallance <contact@harryvallance.com>
2025-12-01 17:32:28 +0000

    Day 1 Part 2

--- 2025/src/Day_01.py
+++ 2025/src/Day_01.py
@@ -13,3 +13,19 @@ for move in values:
         zero_counter += 1
 
 print(f'Part 1: {zero_counter}')
+# Part 1: 1120
+
+# Part 2
+
+dial_pos = 50
+zero_counter = 0
+
+for direction, amount in values:
+    step = -1 if direction == "L" else 1
+    for _ in range(amount):
+        dial_pos = (dial_pos + step) % 100
+        if dial_pos == 0:
+            zero_counter += 1
+
+print(f'Part 2: {zero_counter}')
+# Part 2: 6554
