1e9cf0e0f9a94462e0d21fbd82d029bf98e98684
Harry Vallance <contact@harryvallance.com>
2025-12-04 20:55:19 +0000

    Day 4 Part 2

--- 2025/src/Day_04.py
+++ 2025/src/Day_04.py
@@ -30,3 +30,26 @@ for y in range(len(grid)):
 
 print(f'Part 1: {accessible_rolls}')
 # Part 1: 1587
+
+roll_removed = True
+accessible_rolls = 0
+
+while roll_removed:
+    roll_removed = False
+    for y in range(len(grid)):
+        for x in range(len(grid[y])):
+            if grid[y][x] == '@':
+                total_rolls = 0
+                for dx, dy in directions:
+                    nx = x + dx
+                    ny = y + dy
+                    if 0 <= nx < len(grid[y]) and 0 <= ny < len(grid):
+                        if grid[ny][nx] == '@':
+                            total_rolls += 1
+                if total_rolls < 4:
+                    accessible_rolls += 1
+                    grid[y][x] = '.'
+                    roll_removed = True
+
+print(f'Part 2: {accessible_rolls}')
+# Part 2: 8946
