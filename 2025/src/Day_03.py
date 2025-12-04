545c76238078fd608818ea52021924b0ccc4f14c
Harry Vallance <contact@harryvallance.com>
2025-12-04 19:55:34 +0000

    Day 3 Part 1

--- 2025/src/Day_03.py
+++ 2025/src/Day_03.py
@@ -1,4 +1,16 @@
+import itertools
 from utils import read_file
 
-values = read_file(3, str, True)
+values = read_file(3, str, False)
 
+total_output_joltage = 0
+for bank in values:
+    combinations = []
+    combinations.extend(itertools.combinations(bank, 2))
+    formatted_combinations = []
+    for item in combinations:
+        formatted_combinations.append(int(''.join(item)))
+    total_output_joltage += max(formatted_combinations)
+
+print(f'Part 1: {total_output_joltage}')
+# Part 1: 17113
