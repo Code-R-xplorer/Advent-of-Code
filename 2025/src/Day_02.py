81ddb0318ac67d399a8a77979d5f220111460b96
Harry Vallance <contact@harryvallance.com>
2025-12-04 19:15:37 +0000

    Day 2 Part 2

--- 2025/src/Day_02.py
+++ 2025/src/Day_02.py
@@ -4,17 +4,22 @@ import re
 values = read_file(2, lambda s: s.split(','), False)
 
 product_id_ranges = []
-pat = re.compile(r'^(\d+)\1$')
+
 
 for value in values[0]:
     first_id, last_id = value.split('-')
     product_id_ranges.append((int(first_id), int(last_id)))
 
-invalid_id_total = 0
-for product_id_range in product_id_ranges:
-    for i in range(product_id_range[0], product_id_range[1] + 1):
-        if bool(pat.fullmatch(str(i))):
-            invalid_id_total += i
+def get_invalid_id_total(pattern):
+    invalid_id_total = 0
+    for product_id_range in product_id_ranges:
+        for i in range(product_id_range[0], product_id_range[1] + 1):
+            if bool(pattern.fullmatch(str(i))):
+                invalid_id_total += i
+    return invalid_id_total
 
-print(f'Part 1: {invalid_id_total}')
+print(f'Part 1: {get_invalid_id_total(re.compile(r'^(\d+)\1$'))}')
 # Part 1: 23701357374
+
+print(f'Part 2: {get_invalid_id_total(re.compile(r'^(\d+)\1+$'))}')
+# Part 2: 34284458938
