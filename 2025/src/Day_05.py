cbbf3171ee6a9f22291b3528bff7909eabf466e7
Harry Vallance <contact@harryvallance.com>
2025-12-05 10:34:40 +0000

    Day 5 Part 2

--- 2025/src/Day_05.py
+++ 2025/src/Day_05.py
@@ -14,6 +14,25 @@ for value in values:
     else:
         ids.append(int(value))
 
+
+def merge_ranges(ranges):
+    ranges.sort()
+
+    merged = []
+    current_start, current_end = ranges[0]
+
+    for start, end in ranges[1:]:
+        if start <= current_end + 1:
+            current_end = max(current_end, end)
+        else:
+            merged.append((current_start, current_end))
+            current_start, current_end = start, end
+
+    merged.append((current_start, current_end))
+    return merged
+
+id_ranges = merge_ranges(id_ranges)
+
 total_fresh_ingredients = set()
 for id_range_start, id_range_end in id_ranges:
     for ingredient_id in ids:
@@ -22,3 +41,8 @@ for id_range_start, id_range_end in id_ranges:
 
 print(f'Part 1: {len(total_fresh_ingredients)}')
 # Part 1: 726
+
+fresh_ingredient_ranges = sum(end - start + 1 for start, end in id_ranges)
+
+print(f'Part 2: {fresh_ingredient_ranges}')
+# Part 2: 354226555270043
